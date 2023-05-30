82c23318f8ffa0848600fe6fb3d177f42b0d2840
Sheikhmohammadabdulaziz <132476359+Sheikhmohammadabdulaziz@users.noreply.github.com>
2023-05-30 20:01:00 +0600

    Update login.h

--- login.h
+++ login.h
@@ -1,11 +1,64 @@
+
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
-char name[30];
-char pass[20];
-int amount=1000000,wit_amount,trn_amount,dip_amount;
-double  acu_num=2311113;
+#define MAX_LENGTH 100
+struct info
+{
+    int amount;
+    int wit_amount;
+    int trn_amount;
+    int dip_amount;
+    double acu_num;
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH];
+
+};
+
+ int login()
+{
+    /// Open the data file
+    FILE* file = fopen("information.txt", "r");
+    if (file == NULL) {
+        printf("Error opening data file.\n");
+        return 0;
+    }
+    struct info person;
+    person.amount=1000000;
+    person.acu_num=23111130;
+
+    /// Read username and password from file
+    fscanf(file, "%s %s",person.username , person.password);
+
+    /// Close the data file
+    fclose(file);
+
+    ///Get username and password from the user
+    char inputUsername[MAX_LENGTH];
+    char inputPassword[MAX_LENGTH];
+
+    printf("\n\t\tENTER USERNAME: ");
+    scanf("%s",inputUsername);
+
+    printf("\n\t\tENTER PASSWORD: ");
+    scanf("%s",inputPassword);
+
+    ///Compare the input with stored username and password
+    if (strcmp(person.username,inputUsername) == 0 && strcmp(person.password,inputPassword) == 0)
+    {
+        printf("\n----------------------------------------------------------\n");
+        printf("\t\t<Login successful! please wait>\n");
+
+    }
+
+    else
+
+    {
+        printf("\n\t\t<Invalid username or password>\n");
+        printf("\n\t\t<please try again>");
+        return 0;
 
 
+    }
 
+}
 
