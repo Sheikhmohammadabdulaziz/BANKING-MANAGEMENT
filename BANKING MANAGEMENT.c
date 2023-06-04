dd810a9f0199414605f592979db96ff0e6cc3a4d
Sheikhmohammadabdulaziz <132476359+Sheikhmohammadabdulaziz@users.noreply.github.com>
2023-06-04 21:45:01 +0600

    Update BANKING MANAGEMENT.c

--- BANKING MANAGEMENT.c
+++ BANKING MANAGEMENT.c
@@ -1,19 +1,20 @@
 
 
 
-    #include <stdio.h>                ///ALL LEADER FILE///
-     #include <stdlib.h>
-      #include "login.h"
-       #include<time.h>
-        #include<dos.h>
-         #include"splash screen.h"
-          #include"menu.h"
-           void splash_screen(void);   ///ALL FUNCTIONS///
-          void menu();
-         void deposit_money();
-        void withdrow_money();
-       void transfer_money();
-      void account_details();
-     void transction_details();
-    void last_details();
+           #include <stdio.h>                ///ALL LEADER FILE///
+           #include <stdlib.h>
+           #include "login.h"
+           #include<time.h>
+           #include<dos.h>
+           #include"splash screen.h"
+           #include"menu.h"
+
+           void splash_screen(void);        ///ALL FUNCTIONS///
+           void menu();
+           void deposit_money();
+           void withdrow_money();
+           void transfer_money();
+           void account_details();
+           void transction_details();
+           void last_details();
 
